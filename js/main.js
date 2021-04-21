7622f810407eec2d7164b90ce27464d3e45814bc
Rekku.exe <>
2021-04-21 10:06:05 +0200

    v0.0.8

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 //const worker = require('worker_threads');
 
 function init() {
-    console.log("init");
+    var tab = [];
     for(var i = 1; i<=25; i++){
+        tab.append([]);
         for(var j = 1; j<=10; j++){
+            tab[i-1].append(0);
             $('#board').append("<div id=\"" + j + "-" + i + "\" class=\"box\"></div>");
         }
     }
+    console.log("init");
 }
 
 function sleep(milliseconds) {
