0c64758c820681ca6d6f676403d2d8aa1288d093
Rekku.exe <>
2021-04-21 22:24:15 +0200

    v0.1.3

--- js/clock.js
+++
@@ -1,7 +0,0 @@
-var main = new Worker("js/main.js");
-const date = Date.now();
-let currentDate = null;
-onmessage = function (event) {
-    currentDate = Date.now().seconde;
-    main.postMessage(currentDate);
-}
--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-//const worker = require('worker_threads');
 
 function init(){
     tab = [];
@@ -48,11 +47,124 @@ function ligne(){
     }
 }
 
+function rollForme(){
+    forme = listForme[Math.floor(Math.random() * 7)][0];
+}
+
+function putting(arg){
+    if(forme == "I1"){
+        tab[posY-1][posX] = arg;//   X
+        tab[posY][posX] = arg;//     X
+        tab[posY+1][posX] = arg;//   X
+        tab[posY+2][posX] = arg;//   X
+    } else if(forme == "I2"){
+        tab[posY][posX-1] = arg;//
+        tab[posY][posX] = arg;//    X X X X
+        tab[posY][posX+1] = arg;//
+        tab[posY][posX+2] = arg;//
+    } else if(forme == "L1"){
+        tab[posY-1][posX] = arg;//   X
+        tab[posY][posX] = arg;//     X
+        tab[posY+1][posX] = arg;//   X X
+        tab[posY+1][posX+1] = arg;//
+    } else if(forme == "L2"){
+        tab[posY][posX+1] = arg;//
+        tab[posY][posX] = arg;//     X X X
+        tab[posY][posX-1] = arg;//   X
+        tab[posY+1][posX-1] = arg;//
+    } else if(forme == "L3"){
+        tab[posY-1][posX] = arg;//  X X
+        tab[posY][posX] = arg;//      X
+        tab[posY-1][posX-1] = arg;//  X
+        tab[posY+1][posX] = arg;//
+    } else if(forme == "L4"){
+        tab[posY-1][posX+1] = arg;//
+        tab[posY][posX-1] = arg;//      X
+        tab[posY][posX] = arg;//    X X X
+        tab[posY][posX+1] = arg;//
+    } else if(forme == "l1"){
+        tab[posY-1][posX] = arg;//    X
+        tab[posY][posX] = arg;//      X
+        tab[posY+1][posX] = arg;//  X X
+        tab[posY+1][posX-1] = arg;//
+    } else if(forme == "l2"){
+        tab[posY][posX-1] = arg;//
+        tab[posY][posX] = arg;//     X
+        tab[posY-1][posX-1] = arg;// X X X
+        tab[posY][posX+1] = arg;//
+    } else if(forme == "l3"){
+        tab[posY-1][posX+1] = arg;// X X
+        tab[posY-1][posX] = arg;//   X  
+        tab[posY][posX] = arg;//     X
+        tab[posY+1][posX] = arg;//
+    } else if(forme == "l4"){
+        tab[posY][posX-1] = arg;//
+        tab[posY][posX] = arg;//     X X X
+        tab[posY][posX+1] = arg;//   X
+        tab[posY+1][posX-1] = arg;//
+    } else if(forme == "O1"){
+        tab[posY][posX] = arg;//
+        tab[posY+1][posX] = arg;//    X X
+        tab[posY][posX+1] = arg;//    X X
+        tab[posY+1][posX+1] = arg;//
+    } else if(forme == "S1"){
+        tab[posY][posX+1] = arg;//
+        tab[posY][posX] = arg;//       X X
+        tab[posY+1][posX] = arg;//   X X
+        tab[posY+1][posX-1] = arg;//
+    } else if(forme == "S2"){
+        tab[posY-1][posX] = arg;//    X
+        tab[posY][posX] = arg;//      X X
+        tab[posY][posX+1] = arg;//      X
+        tab[posY+1][posX+1] = arg;//
+    } else if(forme == "Z1"){
+        tab[posY][posX-1] = arg;//
+        tab[posY][posX] = arg;//     X X
+        tab[posY+1][posX] = arg;//     X X
+        tab[posY+1][posX+1] = arg;//
+    } else if(forme == "Z2"){
+        tab[posY-1][posX] = arg;//      X
+        tab[posY][posX] = arg;//      X X
+        tab[posY][posX-1] = arg;//    X
+        tab[posY+1][posX-1] = arg;//
+    } else if(forme == "T1"){
+        tab[posY][posX-1] = arg;//
+        tab[posY][posX] = arg;//     X X X
+        tab[posY][posX+1] = arg;//     X
+        tab[posY+1][posX] = arg;//
+    } else if(forme == "T2"){
+        tab[posY-1][posX] = arg;//    X
+        tab[posY][posX] = arg;//    X X
+        tab[posY+1][posX] = arg;//    X
+        tab[posY][posX-1] = arg;//
+    } else if(forme == "T3"){
+        tab[posY-1][posX] = arg;//
+        tab[posY][posX] = arg;//       X
+        tab[posY][posX-1] = arg;//   X X X
+        tab[posY][posX+1] = arg;//
+    } else if(forme == "T4"){
+        tab[posY-1][posX] = arg;//     X
+        tab[posY][posX] = arg;//       X X
+        tab[posY+1][posX] = arg;//     X
+        tab[posY][posX+1] = arg;//
+    }
+}
+
 posX = 4;
 posY = 0;
+forme = "";
+listForme = [
+    ["I1", "I2", "I1", "I2"],
+    ["L1", "L2", "L3", "I4"],
+    ["l1", "l2", "l3", "l4"],
+    ["O1", "O1", "O1", "O1"],
+    ["S1", "S2", "S1", "S2"],
+    ["Z1", "Z2", "Z1", "Z2"],
+    ["T1", "T2", "T3", "T4"]
+];
 
 document.addEventListener("keydown", function onEvent(e) {
-    tab[posY][posX] = 0;
+    putting(0);
     if(e.key == "ArrowLeft" && posX > 0 && tab[posY][posX -1] == 0){
         posX--;
     } else if(e.key == "ArrowRight" && posX < 9 && tab[posY][posX +1] == 0){
@@ -62,14 +174,9 @@ document.addEventListener("keydown", function onEvent(e) {
     } else if(e.key == "e"){
         window.cancelAnimationFrame( MyGame );
     }
-    tab[posY][posX] = 1;
+    putting(1);
     actu();
 });
-
-/*onmessage = function (event) {
-    console.log(event)
-    
-}*/
 
 tmp = (new Date()).getSeconds();
 
@@ -81,15 +188,17 @@ var tNow = window.performance.now();
         MyGame = window.requestAnimationFrame( main );
 
         if(tmp != (new Date()).getSeconds()){
-            tab[posY][posX] = 0;
+            putting(0);
             if(posY < 24 && tab[posY +1][posX] == 0){
                 posY++;
             } else {
-                tab[posY][posX] = 2;
-                posY = 0;
+                putting(2);
+                posX = 5;
+                posY = 1;
+                rollForme()
                 ligne();
             }
-            tab[posY][posX] = 1;
+            putting(1);
             tmp = (new Date()).getSeconds();
         }
 
