529e2cfb0c1e2c8fd352c14eb374d175884fb367
Rekku.exe <>
2021-04-23 17:53:30 +0200

    v0.3.1

--- js/main.js
+++ js/main.js
@@ -51,9 +51,9 @@ function rollForme(){
     forme = nextForme;
     nextForme = listForme[Math.floor(Math.random() * 7)][0];
     if(nextForme != "l1"){
-        document.getElementById("imgSuivant").innerHTML = "<img id=\"imageSuivant\" src=\"./img/tetromino/" + nextForme + ".png\">";
+        document.getElementById("imgSuivant").innerHTML = "<img id=\"imageSuivant\" src=\"./img/tetromino/" + nextForme + ".PNG\">";
     } else {
-        document.getElementById("imgSuivant").innerHTML = "<img id=\"imageSuivant\" src=\"./img/tetromino/LL1.png\">";
+        document.getElementById("imgSuivant").innerHTML = "<img id=\"imageSuivant\" src=\"./img/tetromino/LL1.PNG\">";
     }
 }
 
