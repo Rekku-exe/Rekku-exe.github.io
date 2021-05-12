fedd59af6359e8e2886ba09a2e6e686ae5d637c8
Rekku.exe <>
2021-05-12 23:04:39 +0200

    animation desactivation fond anime

--- js/index.js
+++ js/index.js
@@ -4,10 +4,14 @@ var actived = true;
 
 function fond(){
     if(actived){
+        document.getElementById("mainIndex").style.right = null;
+        document.getElementById("mainIndex").style.left = null;
         document.getElementById("vid-fond").style.display = "block";
         actived = false;
     } else {
-        document.getElementById("vid-fond").style.display = "none";
+        document.getElementById("mainIndex").style.right = "0";
+        document.getElementById("mainIndex").style.left = "0";
+        setTimeout('document.getElementById("vid-fond").style.display = "none"', 1000);
         actived = true;
     }
 }
