bcb05c00a6d47f9c76e0d9590ce826bbb2eb90b5
Rekku.exe <>
2021-05-14 22:29:12 +0200

    naturo v0.0

--- js/naruto.js
+++ js/naruto.js
@@ -3,8 +3,11 @@ function init(){
 }
 
 function actu(){
+    if(y >= sol) {
+        y = sol;
+    }
+    document.getElementById("player").style.top = y+"px";
     document.getElementById("player").src = "../img/naruto/naruto-"+movement+".png";
-    
 }
 
 var key = {};
@@ -12,7 +15,10 @@ onkeydown = onkeyup = function(e){
     key[e.key] = e.type == 'keydown';
 }
 
+sol = 600;
+y = sol;
 movement = 0;
+acceleration = 0;
 
 tmp = (new Date()).getMilliseconds()/90>>0;
 
@@ -22,10 +28,21 @@ var tNow = window.performance.now();
     init();
     function main( tFrame ) {
         MyGame = window.requestAnimationFrame( main );
-
+        
         if(tmp != (new Date()).getMilliseconds()/90>>0){
             tmp = (new Date()).getMilliseconds()/90>>0;
             movement = (movement+1)%5;
+        }
+        if((key[" "] || key["ArrowUp"]) && y == sol){
+            acceleration = -40;
+        }
+        y+=acceleration;
+        if(y < sol){
+            if((key[" "] || key["ArrowUp"]) && acceleration < 0){
+                acceleration+=2;
+            } else {
+                acceleration+=5;
+            }
         }
         actu();
     }
