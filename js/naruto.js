63677a0d13168f71c7c1b4caebb876f31f2d9622
Rekku.exe <>
2021-05-14 23:07:02 +0200

    naruto anim jump

--- js/naruto.js
+++ js/naruto.js
@@ -5,9 +5,18 @@ function init(){
 function actu(){
     if(y >= sol) {
         y = sol;
+        state = "run";
     }
     document.getElementById("player").style.top = y+"px";
-    document.getElementById("player").src = "../img/naruto/naruto-"+movement+".png";
+    if(state == "run"){
+        document.getElementById("player").src = "../img/naruto/naruto-run-"+movement+".png";
+    } else if(state == "jump") {
+        if(acceleration <= 0){
+            document.getElementById("player").src = "../img/naruto/naruto-jump-0.png";
+        } else {
+            document.getElementById("player").src = "../img/naruto/naruto-jump-1.png";
+        }
+    }
 }
 
 var key = {};
@@ -19,6 +28,7 @@ sol = 600;
 y = sol;
 movement = 0;
 acceleration = 0;
+state = "run";
 
 tmp = (new Date()).getMilliseconds()/90>>0;
 
@@ -35,13 +45,14 @@ var tNow = window.performance.now();
         }
         if((key[" "] || key["ArrowUp"]) && y == sol){
             acceleration = -40;
+            state = "jump";
         }
         y+=acceleration;
         if(y < sol){
             if((key[" "] || key["ArrowUp"]) && acceleration < 0){
                 acceleration+=2;
             } else {
-                acceleration+=5;
+                acceleration+=6;
             }
         }
         actu();
